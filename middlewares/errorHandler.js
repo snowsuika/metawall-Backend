3b32c96e49e4c658818d259888b8a5019e5d35ae
snow <snowsuika@gmail.com>
2022-05-14 22:59:32 +0800

    fix: 修正回傳的錯誤訊息

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 // 開發環境錯誤
 const resErrorDev = (err, res) => {
-    res.status(err.statusCode).json({
+    res.status(err.statusCode);
+    res.send({
         status: err.status,
         message: err.message,
         error: err,
@@ -12,14 +13,16 @@ const resErrorDev = (err, res) => {
 const resErrorProd = (err, res) => {
     // 可預期的錯誤
     if (err.isOperational) {
-        res.status(err.statusCode).json({
+        res.status(err.statusCode);
+        res.send({
             status: 'error',
             message: err.message,
         });
     } else {
         console.error('出現重大錯誤', err);
         // 送出罐頭預設訊息
-        res.status(500).json({
+        res.status(500);
+        res.send({
             status: 'error',
             message: '系統錯誤，請恰系統管理員',
         });
@@ -28,9 +31,9 @@ const resErrorProd = (err, res) => {
 
 // export 錯誤處理 function
 const errorHandler = (err, req, res, next) => {
-    // dev
     err.statusCode = err.statusCode || 500;
 
+    // dev
     switch (process.env.NODE_ENV) {
         case 'dev':
             return resErrorDev(err, res);
