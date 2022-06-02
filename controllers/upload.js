74541f295324b50029f500db1fb02541f4e863f0
snow <snowsuika@gmail.com>
2022-06-03 00:52:50 +0800

    fix: 修正 upload 頭像及一般圖片共用同一個 api

--- controllers/upload.js
+++ controllers/upload.js
@@ -7,32 +7,17 @@ const appError = require('../service/appError');
 
 const upload = {
     uploadImage: async (req, res, next) => {
-        if (!req?.files?.length) return next(new appError('尚未上傳檔案', 400));
+        if (!req?.files?.length) return next(new appError('尚未上傳檔案！', 400));
 
-        const client = new ImgurClient({
-            clientId: process.env.IMGUR_CLIENT_ID,
-            clientSecret: process.env.IMGUR_CLIENT_SECRET,
-            refreshToken: process.env.IMGUR_REFRESH_TOKEN,
-        });
-
-        const response = await client.upload({
-            image: req.files[0].buffer.toString('base64'),
-            type: 'base64',
-            album: process.env.IMGUR_ALBUM_ID,
-        });
-
-        handleSuccess(req, res, {
-            status: 'success',
-            imgUrl: response.data.link,
-        });
-    },
-
-    uploadAvatar: async (req, res, next) => {
-        if (!req?.files?.length) return next(new appError('尚未上傳檔案', 400));
-
+        const { type } = req.body;
         const { width, height } = sizeOf(req.files[0].buffer);
-        const isSquare = width === height;
-        if (!isSquare) return next(new appError('圖片長寬不符合 1:1 尺寸。', 400));
+
+        if (type === 'avatar') {
+            const isSquare = width === height;
+            if (!isSquare) return next(new appError('頭像尺寸必須符合 1:1 比例！', 400));
+        } else {
+            if (width < 300) return next(new appError('圖片寬度不得低於 300 像素！', 400));
+        }
 
         const client = new ImgurClient({
             clientId: process.env.IMGUR_CLIENT_ID,
